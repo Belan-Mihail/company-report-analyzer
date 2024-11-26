7a5c61204d820f04a996f7f4d7c4aa501d81d47f
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-26 21:22:35 +0100

    create upload_file view

--- company_report_analyzer/report_analyzer/views.py
+++ company_report_analyzer/report_analyzer/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import UploadFileForm
 
-# Create your views here.
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+
+            # here can added logic
+            return HttpResponse('File uploaded successfully')
+
+        else:
+            form = UploadFileForm()
+        return render(request, 'upload.html', {'form': form})
+        

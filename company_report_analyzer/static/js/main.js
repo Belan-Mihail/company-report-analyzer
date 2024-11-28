17af13ad3f6c2e095149c27d96cd64ed6ce6b5a0
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-28 10:29:12 +0100

    modify main.js

--- company_report_analyzer/static/js/main.js
+++ company_report_analyzer/static/js/main.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fileInput = document.getElementById('input_field')
     const fileInputSpan = document.getElementById('input_field_span')
+    const subButton = document.getElementById('submit_button')
 
     fileInput.addEventListener('change', () => {
-        fileInputSpan.textContent = fileInput.files[0].name
-        document.getElementById('submit_button').style.display = 'block'
+        if (fileInput.files.lenght > 0) {
+            fileInputSpan.textContent = fileInput.files[0].name
+            subButton.disabled = false 
+        } else {
+            fileInputSpan.textContent = '(File no chhosen)'
+            subButton.disabled = true
+        }
     })
 })

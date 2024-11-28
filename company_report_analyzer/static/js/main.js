2d3d51f71b1c4b23bed2baf4d81a9ebf9664de89
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-28 10:05:47 +0100

    edit addEventListner to show upload button after adding a file

--- company_report_analyzer/static/js/main.js
+++ company_report_analyzer/static/js/main.js
@@ -3,4 +3,5 @@ const fileInputSpan = document.getElementById('input_field_span')
 
 fileInput.addEventListener('change', () => {
     fileInputSpan.textContent = fileInput.files[0].name
+    document.getElementById('submit_button').style.display = 'block'
 })

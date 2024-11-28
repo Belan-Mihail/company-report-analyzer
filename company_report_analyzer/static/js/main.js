086496387f6f0116c9b34f45f01448e0546384a0
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-28 10:13:14 +0100

    add DOMContentLoaded addEventListener to prevent console fail

--- company_report_analyzer/static/js/main.js
+++ company_report_analyzer/static/js/main.js
@@ -1,7 +1,9 @@
-const fileInput = document.getElementById('input_field')
-const fileInputSpan = document.getElementById('input_field_span')
+document.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.getElementById('input_field')
+    const fileInputSpan = document.getElementById('input_field_span')
 
-fileInput.addEventListener('change', () => {
-    fileInputSpan.textContent = fileInput.files[0].name
-    document.getElementById('submit_button').style.display = 'block'
+    fileInput.addEventListener('change', () => {
+        fileInputSpan.textContent = fileInput.files[0].name
+        document.getElementById('submit_button').style.display = 'block'
+    })
 })

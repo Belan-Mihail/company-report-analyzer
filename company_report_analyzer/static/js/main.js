f6c52919e33787715b8e2ff0dc4d6b4099813372
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-11-28 10:00:20 +0100

    add addEventListner to change fileInputSpan text context after upload file

--- company_report_analyzer/static/js/main.js
+++ company_report_analyzer/static/js/main.js
@@ -1,3 +1,6 @@
 const fileInput = document.getElementById('input_field')
 const fileInputSpan = document.getElementById('input_field_span')
 
+fileInput.addEventListener('change', () => {
+    fileInputSpan.textContent = fileInput.files[0].name
+})

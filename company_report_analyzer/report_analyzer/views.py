8700af2a9d1de17a026fe403ca2b3fa3edbcd366
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-02 10:27:34 +0100

    get selected reports from form

--- company_report_analyzer/report_analyzer/views.py
+++ company_report_analyzer/report_analyzer/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UploadFileForm
 
+
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+
+            # We receive the selected reports
+            selected_reports = request.POST.getlist('reports')
 
             # here can added logic
             return HttpResponse('File uploaded successfully')

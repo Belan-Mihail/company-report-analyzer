1dca9a4f80dd75d4a03816b90dba608ebaaa8f73
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-03 16:36:41 +0100

    def generate_pdf function with test code

--- company_report_analyzer/report_analyzer/generate_pdf.py
+++ company_report_analyzer/report_analyzer/generate_pdf.py
@@ -4,6 +4,15 @@ import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
 
 # 1 Loading data from sales_data.csv
-df = pd.read_csv('sales_data.csv')#
+df = pd.read_csv('sales_data.csv')
 
-print(df.head())
+selected_reports = ['report1']
+
+def generate_pdf(df, selected_reports):
+
+    
+    # if first report in selected_reports:
+        if 'report1' in selected_reports:
+            print('hello report1')
+
+generate_pdf(df, selected_reports)

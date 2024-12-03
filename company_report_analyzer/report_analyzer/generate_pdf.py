1d309ab53505f94c13dce550d55c085664e709b2
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-03 16:26:51 +0100

    read test file with data and print head

--- company_report_analyzer/report_analyzer/generate_pdf.py
+++ company_report_analyzer/report_analyzer/generate_pdf.py
@@ -2,3 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
+
+# 1 Loading data from sales_data.csv
+df = pd.read_csv('sales_data.csv')#
+
+print(df.head())

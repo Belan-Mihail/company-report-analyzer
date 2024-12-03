16ca8ff03780d6c2e149cb9dbbeec4c71c940785
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-03 16:40:26 +0100

    add more test code in generate_pdf and test how this function works

--- company_report_analyzer/report_analyzer/generate_pdf.py
+++ company_report_analyzer/report_analyzer/generate_pdf.py
@@ -6,13 +6,41 @@ from matplotlib.backends.backend_pdf import PdfPages
 # 1 Loading data from sales_data.csv
 df = pd.read_csv('sales_data.csv')
 
-selected_reports = ['report1']
+selected_reports = ['report1', 'report3', 'report6', 'report8']
 
 def generate_pdf(df, selected_reports):
 
     
-    # if first report in selected_reports:
-        if 'report1' in selected_reports:
-            print('hello report1')
+    # if report1 in selected_reports:
+    if 'report1' in selected_reports:
+        print('hello report1')
+
+    # if report2 in selected_reports:
+    if 'report2' in selected_reports:
+        print('hello report2')
+    
+    # if report3 in selected_reports:
+    if 'report3' in selected_reports:
+        print('hello report3')
+    
+    # if report4 in selected_reports:
+    if 'report4' in selected_reports:
+        print('hello report4')
+    
+    # if report5 in selected_reports:
+    if 'report5' in selected_reports:
+        print('hello report5')
+
+    # if report6 in selected_reports:
+    if 'report6' in selected_reports:
+        print('hello report6')
+
+    # if report7 in selected_reports:
+    if 'report7' in selected_reports:
+        print('hello report7')
+
+    # if report8 in selected_reports:
+    if 'report8' in selected_reports:
+        print('hello report8')
 
 generate_pdf(df, selected_reports)

8cd7ced65a9a3e52070627c7425040908cfcb335
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-04 08:56:44 +0100

    rotate captions and shorten region names

--- company_report_analyzer/report_analyzer/generate_pdf.py
+++ company_report_analyzer/report_analyzer/generate_pdf.py
@@ -18,11 +18,18 @@ def generate_pdf(df, selected_reports):
     
     # if report1 in selected_reports:
     if 'report1' in selected_reports:
+
+        # group sales by region
         region_sales = df.groupby('Region')['Sales'].sum().sort_values(ascending=False)
-        print(region_sales)
+        
+        # Abbreviation of region names to the first 7 letters
+        region_sales.index = region_sales.index.str.slice(0,7)
+
+        # Plotting a graph
         sns.barplot(x=region_sales.index, y=region_sales.values)
         plt.title('Sales by region')
-        plt.show()
+        plt.xticks(rotation=45) # Rotate captions to improve readability
+        # plt.show()
         
 
     # if report2 in selected_reports:

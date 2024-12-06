3e1399c82fe0a7135b9ac1a2eeb4014c65bd54df
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-06 09:36:26 +0100

    change code vies and success html to avoid error

--- company_report_analyzer/report_analyzer/views.py
+++ company_report_analyzer/report_analyzer/views.py
@@ -9,13 +9,17 @@ from io import BytesIO
 
 def process_file(uploaded_file, selected_reports):
     
+
+     # Преобразуем файл в BytesIO для работы с ним в памяти
+    file_bytes = uploaded_file.read()
+    file_stream = BytesIO(file_bytes)
     
     # Move to the beginning before it convert in df
-    uploaded_file.seek(0)
+    file_stream.seek(0)
     
     
     # Read the uploaded CSV file
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_csv(file_stream)
 
     df = df[:20]
     
@@ -41,6 +45,10 @@ def upload_file(request):
             # Return the PDF as a response
             response = HttpResponse(pdf_output, content_type='application/pdf')
             response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+
+            # Сохраняем PDF в сессии
+            request.session['pdf_output'] = pdf_output.getvalue()
+
             return redirect('success')
         else:
             # Form is invalid, return empty form to display errors
@@ -52,7 +60,21 @@ def upload_file(request):
 
 def success_page(request):
     # Successful processing page
-    return render(request, 'success.html', {
-        'success_message': 'request processed successfully',
-        'show_back_button': True
-    })
+    # Извлекаем PDF данные из сессии и подготавливаем ответ
+    pdf_output = request.session.get('pdf_output')
+    
+    if pdf_output:
+        # Создаем ответ с содержимым PDF
+        response = HttpResponse(pdf_output, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+
+        # Очищаем PDF данные из сессии после отправки ответа
+        del request.session['pdf_output']
+
+        return response
+    else:
+        # Если PDF не найден, просто отображаем страницу успеха
+        return render(request, 'success.html', {
+            'success_message': 'Запрос успешно обработан.',
+            'show_back_button': True
+        })

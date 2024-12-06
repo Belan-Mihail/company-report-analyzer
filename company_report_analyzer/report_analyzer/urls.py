e7ffebe5407ac168aca33840c0485cbcbff7039c
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-06 12:17:19 +0100

    create url for success page

--- company_report_analyzer/report_analyzer/urls.py
+++ company_report_analyzer/report_analyzer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_file, name='upload_file')
+    path('', views.upload_file, name='upload_file'),
+    path('success/', views.succes_page, name='success'),
+
 ]

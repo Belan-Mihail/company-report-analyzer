9f94885e9b8c2f273733c8679cfda18324050c5e
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-10 12:33:41 +0100

    add path to detail page to urls.py

--- company_report_analyzer/report_analyzer/urls.py
+++ company_report_analyzer/report_analyzer/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('success/', views.success, name='success'),
     path('error/', views.error, name='error'),
     path('download/', views.download_pdf, name='download_pdf'),
+    path('detail/', views.detail_page, name='detail_page'),
 ]

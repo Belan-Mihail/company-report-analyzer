d0f4aa536d0034096faf3eb6ef65c502b057d141
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-11 12:27:04 +0100

    add link to download test file

--- company_report_analyzer/company_report_analyzer/settings.py
+++ company_report_analyzer/company_report_analyzer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)x1hc3r)n4=9@ckuqnm^4=5*%ys#9uhcep6v-ozrtd9alqflos
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['company-report-analyzer.onrender.com']
+ALLOWED_HOSTS = ['company-report-analyzer.onrender.com', '127.0.0.1']
 
 
 # Application definition
